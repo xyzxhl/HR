d5a2295432914a373b470805e607b96be94e1c08
sua <107451107+xyzxhl@users.noreply.github.com>
2022-10-01 14:50:05 +0800

    cc

---
+++ Source/HR/Public/HRCreature/HREnemyPawn.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "HRCreature/HRCreaturePawn.h"
+#include "HREnemyPawn.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HR_API AHREnemyPawn : public AHRCreaturePawn
+{
+	GENERATED_BODY()
+	
+};
