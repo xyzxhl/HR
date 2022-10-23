7426786be3afcd392a090fa4034152e18a782a8b
jASONSDFDSA <260831585@qq.com>
2022-10-23 21:46:18 +0800

    AI

---
+++ Source/HR/Private/HRAI/AIBehaviorTree/HRBTTask_SpeedChange.cpp
@@ -0,0 +1,37 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "HRAI/AIBehaviorTree/HRBTTask_SpeedChange.h"
+#include <AIModule/Classes/AIController.h>
+#include <GameFramework/CharacterMovementComponent.h>
+#include <AIModule/Classes/BehaviorTree/BlackboardComponent.h>
+
+EBTNodeResult::Type UHRBTTask_SpeedChange::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	UBlackboardComponent* BBC = OwnerComp.GetBlackboardComponent();
+	AAIController* AIC = OwnerComp.GetAIOwner();
+	if (ensure(AIC))
+	{
+		APawn* AICharacter = Cast<APawn>(AIC->GetPawn());
+		if (ensure(AICharacter))
+		{
+			UCharacterMovementComponent* CMC = Cast<UCharacterMovementComponent>(AICharacter->GetComponentByClass(MovementComp));
+			if (ensure(CMC))
+			{
+				if (BBC->GetValueAsBool(SpeedKey.SelectedKeyName))
+				{
+					CMC->MaxWalkSpeed = 400;
+					return EBTNodeResult::Succeeded;
+				}
+				else
+				{
+					CMC->MaxWalkSpeed = 800;
+					return EBTNodeResult::Succeeded;
+				}
+			}
+			return EBTNodeResult::Failed;
+		}
+		return EBTNodeResult::Failed;
+	}
+	return EBTNodeResult::Failed;
+}
---
+++ Source/HR/Public/HRAI/AIBehaviorTree/HRBTService_CheckSpeed.h
@@ -0,0 +1,30 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTService.h"
+#include <Components/ActorComponent.h>
+#include "HRBTService_CheckSpeed.generated.h"
+
+class UActorComponent;
+class UCharacterMovementComponent;
+class UBlackboardComponent;
+/**
+ *
+ */
+UCLASS()
+class HR_API UHRBTService_CheckSpeed : public UBTService
+{
+	GENERATED_BODY()
+
+protected:
+
+	UPROPERTY(EditAnywhere, Category = "KeySelector")
+		FBlackboardKeySelector SpeedCheckKey;
+
+	UPROPERTY(EditAnywhere, Category = "AIComponents")
+		TSubclassOf<UActorComponent> MovementComp;
+
+	virtual void TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds) override;
+};
