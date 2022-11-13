6e7a67dfc3f4dd94b63aabc255717d8ec9165240
jASONSDFDSA <260831585@qq.com>
2022-11-13 23:17:04 +0800

    CC

---
+++ Source/HR/Private/Miscellaneous/HRWorldUserWidget.cpp
@@ -0,0 +1,37 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Miscellaneous/HRWorldUserWidget.h"
+#include <Kismet/GameplayStatics.h>
+#include <Blueprint/WidgetLayoutLibrary.h>
+#include <Components/SizeBox.h>
+
+
+void UHRWorldUserWidget::NativeTick(const FGeometry& MyGeometry, float InDeltaTime)
+{
+	Super::NativeTick(MyGeometry, InDeltaTime);
+
+	// Project enemy's world location to viewport.
+
+	if (!IsValid(ActorToAttach))
+	{
+		RemoveFromParent();
+
+		UE_LOG(LogTemp, Warning, TEXT("Actor is no longer valid."));
+		return;
+	}
+
+	FVector2D ScreenPosition;
+
+	if (UGameplayStatics::ProjectWorldToScreen(GetOwningPlayer(), ActorToAttach->GetActorLocation() + WorldOffset, ScreenPosition))
+	{
+		float Scale = UWidgetLayoutLibrary::GetViewportScale(this);
+
+		ScreenPosition /= Scale;
+
+		if (ParentSizeBox)
+		{
+			ParentSizeBox->SetRenderTranslation(ScreenPosition);
+		}
+	}
+}
